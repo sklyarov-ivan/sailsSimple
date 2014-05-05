2212ba052ebd27552b5f782d33edaad10288b1fc
Ivan Sklyarov <sklyarov.161@gmail.com>
2014-05-06 00:19:17 +0400

    add validation

---
+++ api/controllers/UserController.js
@@ -0,0 +1,48 @@
+/**
+ * UserController
+ *
+ * @module      :: Controller
+ * @description	:: A set of functions called `actions`.
+ *
+ *                 Actions contain code telling Sails how to respond to a certain type of request.
+ *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+ *
+ *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+ *                 and/or override them with custom routes (`config/routes.js`)
+ *
+ *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
+ *
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+module.exports = {
+    
+  
+  new : function(req,res){
+    res.locals.flash = _.clone(req.session.flash);
+      // console.log(res.locals.flash);
+    res.view();
+    req.session.flash = {};
+  },
+  create: function(req,res,next){
+    User.create(req.params.all(),function userCreated(err, user){
+      if(err) {
+        req.session.flash = {
+          err:err
+        }
+        return res.redirect('/user/new');
+      }
+
+      res.json(user);
+      res.session.flash = {};
+    })
+  },
+
+  /**
+   * Overrides for the settings in `config/controllers.js`
+   * (specific to UserController)
+   */
+  _config: {}
+
+  
+};
