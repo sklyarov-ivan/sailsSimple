5a0ae1df57038ba65ff62cd5b687a097738f3cfa
Ivan Sklyarov <sklyarov.161@gmail.com>
2014-05-06 11:30:42 +0400

    add crud functionality

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -26,15 +26,60 @@ module.exports = {
       if(err) {
         req.session.flash = {
           err:err
-        }
+        };
         return res.redirect('/user/new');
       }
 
-      res.json(user);
+      // res.json(user);
       // res.session.flash = {};
-    })
+      res.redirect('/user/show/'+user.id);
+    });
   },
+  show: function(req,res,next){
+    User.findOne(req.param('id'), function foundUser(err,user){
+      if (err) return next(err);
+      if (!user) return next('User dosn\'t exist');
+      res.view({
+        user:user
+      });
+    });
+  },
+  edit: function(req,res,next){
+    User.findOne(req.param('id'), function userFound(err,user){
+      if (err) return next(err);
+      if (!user) return next('User dosn\'t exist');
+      res.view({
+        user:user
+      });
+    });
+  },
+  update: function(req,res,next){
+    User.update(req.param('id'),req.params.all(),function userUpdated(err){
+      if (err){
+        return res.redirect('/user/edit/'+req.param('id'));
+      }
+      res.redirect('/user/show/'+req.param('id'));
+    });
+  },
+  destroy: function(req,res,next){
+    User.findOne(req.param('id'), function userFound(err,user){
+      if (err) return next(err);
+      if (!user) return next('User dosn\'t exist');
 
+      User.destroy(req.param('id'),function userDestroyed(err){
+        if (err) return next(err);
+        return res.redirect('/user');
+      });
+    });
+  },
+  index: function(req,res,next){
+    User.find(function usersFound(err,users){
+      if (err) return next(err);
+      res.view({
+        users:users
+      });
+    });
+  },
   /**
    * Overrides for the settings in `config/controllers.js`
    * (specific to UserController)
